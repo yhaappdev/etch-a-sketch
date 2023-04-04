d730026416e423f723dcdb31a425acadcfe4d154
yhaappdev <121143197+yhaappdev@users.noreply.github.com>
2023-04-04 16:34:58 +0800

    addition of button functions

--- scripts.js
+++ scripts.js
@@ -1,24 +1,54 @@
-const divContainer = document.querySelector('#div-grid-container');
-// const divGrid = document.createElement('div');
-// divGrid.classList.add('div');
-// divGrid.style.backgroundColor = 'pink';
-// divGrid.textContent = "Test";
-// divContainer.appendChild(divGrid);
+const DEFAULT_SIZE = 16;
 
-function generateGrid(rows, cols){
-    divContainer.style.setProperty('--grid-rows', rows);
-    divContainer.style.setProperty('--grid-cols', cols);
-    for(c = 0; c < (rows * cols); c++){
+let currentSize = DEFAULT_SIZE;
+const divContainer = document.querySelector('#div-grid-container');
+const setContainer = document.querySelector('#setting-container');
+const changeBtn = document.createElement("Button");
+changeBtn.setAttribute('style', 'border: none; color: black; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px;');
+changeBtn.appendChild(document.createTextNode("Change Grid Layout"));
+setContainer.appendChild(changeBtn);
+
+function setCurrentSize(newSize){
+    currentSize = newSize;
+}
+
+function generateGrid(size){
+    divContainer.style.setProperty('--grid-rows', size);
+    divContainer.style.setProperty('--grid-cols', size);
+    for(c = 0; c < (size * size); c++){
         let cell = document.createElement("div");
         cell.innerText = (c + 1);
         divContainer.appendChild(cell).className = "grid-item";
     };
 };
 
-function hover(element, className){
-    element.addEventListener('mouseenter', e => element.classList.add(className));
-    element.addEventListener('mouseleave', e => element.classList.remove(className));
+//event delegation
+function toggleTrail(event){
+    if( !event.target.matches('.grid-item')) return;
+    event.target.classList.add('my-color-class');
 }
 
-hover(divContainer, 'button:hover');
-generateGrid(16,16);
+function reloadGrid(){
+    clearGrid();
+    generateGrid(currentSize);
+}
+
+function clearGrid(event){
+    divContainer.innerHTML = '';
+}
+
+function onClick(event){
+    var value = prompt("Enter Grid Format");
+    var sizeValue = parseInt(value);
+    console.log(sizeValue * sizeValue);
+    setCurrentSize(sizeValue);
+    reloadGrid();
+}
+
+window.onload = () => {
+    generateGrid(DEFAULT_SIZE);
+}
+changeBtn.addEventListener('click', onClick);
+divContainer.addEventListener('mouseover', toggleTrail);
+
+
